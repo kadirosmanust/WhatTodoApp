23fae62bfdeca23464f7e7cd8c5e21685653cfed
Kadir Osman Üst <92394525+KadoRaw@users.noreply.github.com>
2022-04-27 13:45:22 +0300

    profilehandled (#4)

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,10 +1,9 @@
 import Router from 'next/router';
 import React, { useEffect } from 'react';
+import ProfileCard from '../../src/components/ProfileCard/ProfileCard';
 import { useAppSelector } from '../../src/store/store';
 
-type Props = {};
-
-const UserProfile = (props: Props) => {
+const UserProfile = () => {
   const { isRegistered, username } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
@@ -14,7 +13,11 @@ const UserProfile = (props: Props) => {
     }
   }, [isRegistered]);
 
-  return <div>{username}</div>;
+  return (
+    <div className='center'>
+      <ProfileCard username={username} />
+    </div>
+  );
 };
 
 export default UserProfile;
