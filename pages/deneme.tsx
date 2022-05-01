f79d8a0b27b3864019b9e7b15b84d4811f6118be
Kadir Osman Üst <92394525+KadoRaw@users.noreply.github.com>
2022-05-01 12:27:23 +0300

    Added auto save and rich text editor.

---
+++ pages/deneme.tsx
@@ -0,0 +1,8 @@
+import Router from 'next/router';
+import TextEditor from '../src/components/TextEditor/TextEditor';
+
+const Deneme = () => {
+  return <TextEditor />;
+};
+
+export default Deneme;
